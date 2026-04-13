492edcf591e68fbbae74ddefaacd03662fad3256
anfiil13 <nastagireva315@gmail.com>
2026-04-14 01:10:13 +0300

    Add files via upload

---
+++ src/Triangle.java
@@ -0,0 +1,22 @@
+public class Triangle extends Shape {
+
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        super(ShapeType.TRIANGLE);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+
+    @Override
+    public double getArea() {
+        double p = getPerimeter() / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
